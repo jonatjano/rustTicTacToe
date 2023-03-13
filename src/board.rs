28cca3e45d0361b2aae73718df0be57944a9293e
jselle <jselle@greensystemes.com>
2023-03-13 17:07:40 +0100

    cargo fmt + cargo clippy

--- src/board.rs
+++ src/board.rs
@@ -1,80 +1,100 @@
+use std::fmt::{Display, Formatter};
+
 #[derive(PartialEq, Copy, Clone)]
 pub enum CellState {
-    EMPTY,
-    CIRCLE,
-    CROSS
+    Empty,
+    Circle,
+    Cross,
 }
 
 impl CellState {
-    pub fn to_string(&self) -> String {
+    fn to_str(&self) -> &'static str {
         match self {
-            CellState::EMPTY => String::from(" "),
-            CellState::CIRCLE => String::from("O"),
-            CellState::CROSS => String::from("X")
+            CellState::Empty => " ",
+            CellState::Circle => "O",
+            CellState::Cross => "X",
         }
     }
 }
 
 pub struct Board {
     pub board: [CellState; 9],
-    pub first_player_turn: bool
+    pub first_player_turn: bool,
 }
 
 impl Board {
     pub(crate) fn ended(&self) -> bool {
-        return self.winner() != CellState::EMPTY || self.board.iter().all(|cell| cell != &CellState::EMPTY)
+        return self.winner() != CellState::Empty
+            || self.board.iter().all(|cell| cell != &CellState::Empty);
     }
 
     pub(crate) fn winner(&self) -> CellState {
         // row
-        if self.board[0] == self.board[1] && self.board[0] == self.board[2] { self.board[0] }
-        else if self.board[3] == self.board[4] && self.board[3] == self.board[5] { self.board[3] }
-        else if self.board[6] == self.board[7] && self.board[6] == self.board[8] { self.board[6] }
+        if self.board[0] == self.board[1] && self.board[0] == self.board[2] {
+            self.board[0]
+        } else if self.board[3] == self.board[4] && self.board[3] == self.board[5] {
+            self.board[3]
+        } else if self.board[6] == self.board[7] && self.board[6] == self.board[8] {
+            self.board[6]
+        }
         // col
-        else if self.board[0] == self.board[3] && self.board[0] == self.board[6] { self.board[0] }
-        else if self.board[1] == self.board[4] && self.board[1] == self.board[7] { self.board[3] }
-        else if self.board[2] == self.board[5] && self.board[2] == self.board[8] { self.board[6] }
+        else if self.board[0] == self.board[3] && self.board[0] == self.board[6] {
+            self.board[0]
+        } else if self.board[1] == self.board[4] && self.board[1] == self.board[7] {
+            self.board[3]
+        } else if self.board[2] == self.board[5] && self.board[2] == self.board[8] {
+            self.board[6]
+        }
         //diag
-        else if self.board[0] == self.board[4] && self.board[0] == self.board[8] { self.board[0] }
-        else if self.board[2] == self.board[4] && self.board[2] == self.board[6] { self.board[2] }
-
-        else { CellState::EMPTY }
+        else if self.board[0] == self.board[4] && self.board[0] == self.board[8] {
+            self.board[0]
+        } else if self.board[2] == self.board[4] && self.board[2] == self.board[6] {
+            self.board[2]
+        } else {
+            CellState::Empty
+        }
     }
 
     pub(crate) fn play(&mut self) {
         use std::io;
         let mut i = 0;
-        println!("{}'s turn", if self.first_player_turn { "Cross" } else { "Circle" });
+        println!(
+            "{}'s turn",
+            if self.first_player_turn {
+                "Cross"
+            } else {
+                "Circle"
+            }
+        );
         println!("Select where to play (1-9)");
         loop {
             let stdin = io::stdin();
             let mut user_input = String::new();
             match stdin.read_line(&mut user_input) {
                 Ok(_n) => {}
-                Err(e) => println!("{}", e)
+                Err(e) => println!("{}", e),
             };
             match user_input.trim().parse() {
-                Ok(n) => { i = n; }
-                Err(e) => println!("{}", e)
+                Ok(n) => {
+                    i = n;
+                }
+                Err(e) => println!("{}", e),
             };
-            if i < 1 || i > 9 { continue; }
-            if self.board[i-1] == CellState::EMPTY { break; }
+            if !(1..=9).contains(&i) {
+                continue;
+            }
+            if self.board[i - 1] == CellState::Empty {
+                break;
+            }
             println!("This cell is already used select another one (1-9)");
         }
         println!("Playing on cell {}", i);
-        self.board[i-1] = if self.first_player_turn { CellState::CROSS } else { CellState::CIRCLE };
-        self.first_player_turn = ! self.first_player_turn;
-    }
-
-    fn to_string(&self) -> String {
-        let mut str = String::from("+---+---+---+\n");
-        for (i, cell) in self.board.iter().enumerate() {
-            str += &(String::from("| ") + cell.to_string().as_str() + " ");
-            if i % 3 == 2 {
-                str += "|\n+---+---+---+\n";
-            }
-        }
-        return str.to_string();
+        self.board[i - 1] = if self.first_player_turn {
+            CellState::Cross
+        } else {
+            CellState::Circle
+        };
+        self.first_player_turn = !self.first_player_turn;
     }
 }
 
@@ -82,17 +102,30 @@ impl Default for Board {
     fn default() -> Board {
         Board {
             board: [
-                CellState::EMPTY, CellState::EMPTY, CellState::EMPTY,
-                CellState::EMPTY, CellState::EMPTY, CellState::EMPTY,
-                CellState::EMPTY, CellState::EMPTY, CellState::EMPTY
+                CellState::Empty,
+                CellState::Empty,
+                CellState::Empty,
+                CellState::Empty,
+                CellState::Empty,
+                CellState::Empty,
+                CellState::Empty,
+                CellState::Empty,
+                CellState::Empty,
             ],
-            first_player_turn: true
+            first_player_turn: true,
         }
     }
 }
 
-impl std::fmt::Display for Board {
-    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(fmt, "{}", self.to_string())
+impl Display for Board {
+    fn fmt(&self, fmt: &mut Formatter<'_>) -> std::fmt::Result {
+        writeln!(fmt, "+---+---+---+")?;
+        for (i, cell) in self.board.iter().enumerate() {
+            write!(fmt, "| {} ", cell.to_str())?;
+            if i % 3 == 2 {
+                fmt.write_str("|\n+---+---+---+\n")?;
+            }
+        }
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,20 @@ mod board;
 use crate::board::*;
 
 fn main() {
-    let mut board: Board = Board{..Default::default()};
-    while ! board.ended() {
+    let mut board: Board = Board {
+        ..Default::default()
+    };
+    while !board.ended() {
         print!("{}", board);
         board.play();
     }
     print!("{}", board);
-    println!("Winner is {}", match board.winner() {
-        CellState::EMPTY => "Nobody",
-        CellState::CIRCLE => "Circle",
-        CellState::CROSS => "Cross"
-    });
+    println!(
+        "Winner is {}",
+        match board.winner() {
+            CellState::Empty => "Nobody",
+            CellState::Circle => "Circle",
+            CellState::Cross => "Cross",
+        }
+    );
 }
